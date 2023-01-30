c3c148b6beb567baedd370acad2947a4fa68d46e
Fabiana <fabiana.liria@udla.edu.ec>
2023-01-29 22:47:01 -0500

    Se añade la clase AreaVisitor

---
+++ Models/AreaVisitor.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplicationVisitor.Models
+{
+    public class AreaVisitor : IVisitor
+    {
+        public double TotalArea { get; set; }
+
+        public void Visit(Circle circle)
+        {
+            TotalArea += Math.PI * Math.Pow(circle.Radius, 2);
+        }
+
+        public void Visit(Rectangle rectangle)
+        {
+            TotalArea += rectangle.Width * rectangle.Height;
+        }
+    }
+}
---
+++ Models/ShapeContainer.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplicationVisitor.Models
+{
+    class Client
+    {
+        private List<IShape> _shapes = new List<IShape>();
+        private AreaVisitor _visitor = new AreaVisitor();
+
+        public Client()
+        {
+            _shapes.Add(new Circle(2));
+            _shapes.Add(new Rectangle(3, 4));
+            _shapes.Add(new Circle(5));
+        }
+
+        public void Run()
+        {
+            foreach (var shape in _shapes)
+            {
+                shape.Accept(_visitor);
+            }
+            Console.WriteLine("Total area: " + _visitor.TotalArea);
+        }
+    }
+}
