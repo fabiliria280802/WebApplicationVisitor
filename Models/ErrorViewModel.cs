f3b96f6c3e6a2a140b17bfe9e8b8d0a1e9fa882a
Fabiana <fabiana.liria@udla.edu.ec>
2023-01-29 20:29:05 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationVisitor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
