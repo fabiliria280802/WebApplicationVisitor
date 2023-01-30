a0db66a1ee1c7630e39defac2d7ccf5803dbe6ca
Fabiana <fabiana.liria@udla.edu.ec>
2023-01-29 22:59:46 -0500

    Se reviso las fallas de Visitor

---
+++ Models/Startup.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplicationVisitor.Models
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var client = new Client();
+            client.Run();
+        }
+    }
+}
--- Models/Visitor.cs
+++ Models/Visitor.cs
@@ -1,18 +1,40 @@
-﻿using System.Runtime.ConstrainedExecution;
+﻿using Microsoft.AspNetCore.Rewrite;
+using System.Drawing;
+using System.Runtime.ConstrainedExecution;
 using System.Runtime.Intrinsics.X86;
 
 namespace WebApplicationVisitor.Models
 {
     public class Visitor : IVisitor
     {
-        public void VisitRectangle(Rectangle rectangle)
+        public class AreaVisitor
         {
-            Console.WriteLine("Area of Rectangle: " + rectangle.Width * rectangle.Height);
+            public double TotalArea { get; set; }
+
+            public void VisitCircle(Circle circle)
+            {
+                TotalArea += Math.PI * Math.Pow(circle.Radius, 2);
+            }
+
+            public void VisitRectangle(Rectangle rectangle)
+            {
+                TotalArea += rectangle.Height * rectangle.Width;
+            }
         }
 
-        public void VisitCircle(Circle circle)
+        public class PerimeterVisitor
         {
-            Console.WriteLine("Area of Circle: " + Math.PI * circle.Radius * circle.Radius);
+            public double TotalPerimeter { get; set; }
+
+            public void VisitCircle(Circle circle)
+            {
+                TotalPerimeter += 2 * Math.PI * circle.Radius;
+            }
+
+            public void VisitRectangle(Rectangle rectangle)
+            {
+                TotalPerimeter += 2 * (rectangle.Height + rectangle.Width);
+            }
         }
     }
 }
